1381fad99fda864057a9eede47a2b745bf962699
yaji33 <yajii3835@gmail.com>
2025-06-22 01:58:39 +0800

    chore: add config to remove console for prod

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -37,7 +37,10 @@ export default tseslint.config(
           endOfLine: 'auto',
         },
       ],
-      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
     },
   },
   prettier
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
   },
 });
